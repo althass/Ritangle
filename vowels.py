5da7768ade4df3b81dcffdbe1ee9dba87e7f133f
althass <hassanmaybeme@gmail.com>
2024-10-30 13:49:03 +0000

    Added code to brute force some clues

--- vowels.py
+++ vowels.py
@@ -1,4 +1,14 @@
-sentance = "Count carefully and you will see that this sentence contains nineteen words and between them they contain twenty seven vowels."
+sqr = [1,4,9,16,25,36,49,64,81,100]
+arr = []
+for number in range(10,100):
+    strnum = str(number)
+    if int(strnum[0]) + int(strnum[1]) in sqr:
+        arr.append(strnum)
+print(arr)
 
-sentance.split()
-print(sentance)
+array = []
+i = 0
+while len(str(i**3)) < 3:
+    array.append(i**3)
+    i +=1
+print(array)
